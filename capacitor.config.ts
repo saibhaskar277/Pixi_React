e62ba9decfe661602f2ab7d3dd305eb330950bd2
saibhaskar277 <sai.bhaskar277@gmail.com>
2026-05-14 01:24:54 +0530

    cross platform support to android (Capacitor) and dev scripts to package.json

---
+++ capacitor.config.ts
@@ -0,0 +1,15 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.yourcompany.pixireactgame",
+
+  appName: "Pixi React Game",
+
+  webDir: "dist",
+
+  android: {
+    backgroundColor: "#000000",
+  },
+};
+
+export default config;
